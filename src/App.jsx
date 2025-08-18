47e030cc056a43602684de01a2e3950305e5f555
Ujwala Shetty <ujwalashetty660@gmail.com>
2025-08-19 01:48:57 +0530

    Added Contacts section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar.jsx'
 import Hero from './components/hero/hero.jsx'
+import Title from './components/title/title.jsx'
 import Footer from './components/footer/footer.jsx'
+import Contacts from "./components/contacts/contacts.jsx";
 
 const App = () => {
   return (
     <div>
-      <Navbar />  
+      <Navbar />
       <Hero />
-      <Footer />
+
+      <div className="container">
+        <Title subTitle="Contact US" title="Get in Touch" />
+        <Contacts />
+        <Footer />
       </div>
-  )
+    </div>
+  );
 }
 
 export default App
