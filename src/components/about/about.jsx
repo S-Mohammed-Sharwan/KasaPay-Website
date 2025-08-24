390af5e79bb711f86cf5e78f9a995a4db07592cd
Ujwala Shetty <ujwalashetty660@gmail.com>
2025-08-24 15:16:06 +0530

    Updated about section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -12,7 +12,7 @@ const about = () => {
         <img src="" alt="" />
       </div>
       <div className="about-right">
-        <h3 >About KasaPay</h3>
+        <h3>About KasaPay</h3>
         <h2>Simplifying Waste Collection Payments</h2>
         <p>
           KasaPay is a specialized payment platform designed specifically for
@@ -33,11 +33,48 @@ const about = () => {
         <div className="highlight-box">Detailed Analytics</div>
       </div>
       <div className="about-imgs">
-        <div className="highlight-img"><img src={aboutimg1} /></div>
-        <div className="highlight-img"><img src={aboutimg2} /></div>
-        <div className="highlight-img"><img src={aboutimg3} /></div>
-        <div className="highlight-img"><img src={aboutimg4} /></div>
+        <div className="highlight-img">
+          <img src={aboutimg1} />
+        </div>
+        <div className="highlight-img">
+          <img src={aboutimg2} />
+        </div>
+        <div className="highlight-img">
+          <img src={aboutimg3} />
+        </div>
+        <div className="highlight-img">
+          <img src={aboutimg4} />
+        </div>
       </div>
+      <section class="section">
+        <h2>Did You Know?</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Waste Type</th>
+              <th>Estimated Daily Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Municipal Solid Waste</td>
+              <td>~320 tons generated / ~245 tons collected</td>
+            </tr>
+            <tr>
+              <td>Sanitary Waste</td>
+              <td>5–10 tons</td>
+            </tr>
+            <tr>
+              <td>Biomedical Waste</td>
+              <td>~14.5 tons</td>
+            </tr>
+            <tr>
+              <td>E-waste</td>
+              <td>~2 tons</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     </div>
   );
 }
